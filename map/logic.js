0b57d8f5e0ee4a3e324727e34a9b9e2b84650acd
Karthik <ks.the.visionary@gmail.com>
2024-10-19 14:56:27 +0530

    Updated route tracking logic

--- map/logic.js
+++ map/logic.js
@@ -5,6 +5,15 @@ const search_string = window.location.search;
 const query_params = new URLSearchParams(search_string);
 let marker;
 
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 
 var options_for_map = {
     enableHighAccuracy: true,
@@ -52,7 +61,7 @@ function update_marker(position) {
     } catch(e) {
         console.log("Couldn't remove old location");
     }
-    marker = new L.Marker([position.coords.latitude, position.coords.longitude]);
+    marker = new L.Marker([position.coords.latitude, position.coords.longitude], {icon: greenIcon});
     marker.addTo(map);
     marker.bindPopup("You are here");
 
